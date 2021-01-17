f1f120a84d1fefdf1402ecc0705d59c0ebe26750
kazimirm <kazimir.majo@gmail.com>
2021-01-17 18:17:02 +0100

    types added

--- src/com/kazimirm/hddlParser/hddlObjects/Type.java
+++ src/com/kazimirm/hddlParser/hddlObjects/Type.java
@@ -1,4 +1,25 @@
 package com.kazimirm.hddlParser.hddlObjects;
 
 public class Type {
+    private String name;
+    private String baseType;
+
+    public Type() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBaseType() {
+        return baseType;
+    }
+
+    public void setBaseType(String baseType) {
+        this.baseType = baseType;
+    }
 }
