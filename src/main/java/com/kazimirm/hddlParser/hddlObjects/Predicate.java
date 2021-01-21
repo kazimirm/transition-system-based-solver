f16b498e5e1a620ef8799824abc6fd45ac508bd5
kazimirm <kazimir.majo@gmail.com>
2021-01-21 17:22:50 +0100

    predicates added

--- src/main/java/com/kazimirm/hddlParser/hddlObjects/Predicate.java
+++ src/main/java/com/kazimirm/hddlParser/hddlObjects/Predicate.java
@@ -1,4 +1,34 @@
 package com.kazimirm.hddlParser.hddlObjects;
 
+import java.util.List;
+
 public class Predicate {
+    private String name;
+    private List<Argument> arguments;
+
+    public Predicate() {
+    }
+
+    public Predicate(String name, List<Argument> arguments) {
+        this.name = name;
+        this.arguments = arguments;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Argument> getArguments() {
+        return arguments;
+    }
+
+    public void setArguments(List<Argument> arguments) {
+        this.arguments = arguments;
+    }
 }
+
+
