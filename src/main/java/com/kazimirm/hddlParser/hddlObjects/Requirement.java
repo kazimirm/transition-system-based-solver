05621a4e1a3aa85d6216ca5b8ee47a83385939f9
kazimirm <kazimir.majo@gmail.com>
2021-01-22 00:29:45 +0100

    Requirement and token added

--- src/main/java/com/kazimirm/hddlParser/hddlObjects/Requirement.java
+++ src/main/java/com/kazimirm/hddlParser/hddlObjects/Requirement.java
@@ -1,4 +1,20 @@
 package com.kazimirm.hddlParser.hddlObjects;
 
 public class Requirement {
+    private String value;
+
+    public Requirement(String value) {
+        this.value = value;
+    }
+
+    public Requirement() {
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
