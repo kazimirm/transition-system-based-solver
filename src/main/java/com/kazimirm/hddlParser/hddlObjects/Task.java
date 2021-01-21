60f11ecd1fa87eeaa15a1a68dc5e50feaa6ee47c
kazimirm <kazimir.majo@gmail.com>
2021-01-22 00:59:45 +0100

    task class added

--- src/main/java/com/kazimirm/hddlParser/hddlObjects/Task.java
+++ src/main/java/com/kazimirm/hddlParser/hddlObjects/Task.java
@@ -1,4 +1,32 @@
 package com.kazimirm.hddlParser.hddlObjects;
 
+import java.util.List;
+
 public class Task {
+    private String name;
+    private List<Argument> parameters;
+
+    public Task() {
+    }
+
+    public Task(String name, List<Argument> parameters) {
+        this.name = name;
+        this.parameters = parameters;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Argument> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<Argument> parameters) {
+        this.parameters = parameters;
+    }
 }
