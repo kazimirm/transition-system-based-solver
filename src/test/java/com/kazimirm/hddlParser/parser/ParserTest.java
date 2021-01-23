9eb6b9fdeb8553bc9b9ca29647190e3a2550ab51
kazimirm <kazimir.majo@gmail.com>
2021-01-23 17:58:34 +0100

    negative tests added

--- src/test/java/com/kazimirm/hddlParser/parser/ParserTest.java
+++ src/test/java/com/kazimirm/hddlParser/parser/ParserTest.java
@@ -13,6 +13,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ParserTest {
 
     private final String BASIC_INPUT_FILE = "basic_input.txt";
+    private final String BASIC_INPUT_FILE_INVALID_1 = "basic_input_invalid_1.txt"; // unexpected text after requirements
+    private final String BASIC_INPUT_FILE_INVALID_2 = "basic_input_invalid_1.txt"; // wrong space
+
     @Test
     public void testParser_basicInput_valid() throws FileNotFoundException, ParseException {
         ClassLoader classLoader = this.getClass().getClassLoader();
@@ -29,4 +32,25 @@ public class ParserTest {
         assertEquals(5, domain.getPredicates().size());
         assertEquals(4, domain.getTasks().size());
     }
+
+    @Test
+    public void testParser_basicInput_invalid_1() throws FileNotFoundException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        File file = new File(classLoader.getResource(BASIC_INPUT_FILE_INVALID_1).getFile());
+        InputStream targetStream = new FileInputStream(file);
+
+        ParserHDDL parser = new ParserHDDL(targetStream);
+        assertThrows(ParseException.class, () -> parser.start());
+    }
+
+    @Test
+    public void testParser_basicInput_invalid_2() throws FileNotFoundException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        File file = new File(classLoader.getResource(BASIC_INPUT_FILE_INVALID_2).getFile());
+        InputStream targetStream = new FileInputStream(file);
+
+        ParserHDDL parser = new ParserHDDL(targetStream);
+        assertThrows(ParseException.class, () -> parser.start());
+    }
+
 }
